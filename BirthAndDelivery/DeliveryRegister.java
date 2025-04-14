13545da1796b632b00f09de5eb677653141b5ca0
Abigail Pitcairn <143832183+abbyPitcairn@users.noreply.github.com>
2025-04-14 15:06:22 -0400

    DeliveryRegister update

--- BirthAndDelivery/DeliveryRegister.java
+++ BirthAndDelivery/DeliveryRegister.java
@@ -1,1 +1,14 @@
+package Archive.Iteration_2;
 
+public class DeliveryRegister {
+
+    
+
+    public void addRecord(DeliveryRecord record){
+
+    }
+    public void deleteRecord(){};
+    public void editRecord(){};
+    public void printRecord(DeliveryRecord record){};
+
+}
